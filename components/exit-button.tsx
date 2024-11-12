2cdb6218cfea7a945c6bda5e7fa60edda51a511b
cjy <1404340013@qq.com>
2024-11-12 17:59:03 +0800

    feat:退出登录逻辑实现

--- components/exit-button.tsx
+++ components/exit-button.tsx
@@ -1,37 +1,33 @@
-"use client";
+// "use client";
 import { Avatar } from "@nextui-org/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover";
 import { IoMdExit } from "react-icons/io";
+import { Button } from "@nextui-org/button";
 
-// import { signOut } from "@/auth";
+import { signOut } from "@/auth";
 
 const ExitButton = ({ name }: { name: string }) => {
-  const handleExit = async () => {
-    // "use server";
-    // await signOut();
-  };
-
   return (
     <div className="">
       <Popover placement="bottom">
         <PopoverTrigger>
-          <Avatar isBordered className="h-10 w-10 cursor-pointer" name={name} />
+          <Avatar isBordered className="h-8 w-8 cursor-pointer" name={name} />
         </PopoverTrigger>
         <PopoverContent>
-          <div
-            className="flex cursor-pointer items-center gap-2 px-1 py-2 text-zinc-600"
-            role="button"
-            tabIndex={0}
-            onClick={handleExit}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" || e.key === " ") {
-                handleExit();
-              }
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
             }}
           >
-            <p className="font-bold">退出</p>
-            <IoMdExit className="text-2xl" />
-          </div>
+            <Button
+              className="bg-white"
+              endContent={<IoMdExit className="text-2xl" />}
+              type="submit"
+            >
+              退出
+            </Button>
+          </form>
         </PopoverContent>
       </Popover>
     </div>
