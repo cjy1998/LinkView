27f94e570dfa6ed59a4fc6d6b1b26958b19d7220
cjy <1404340013@qq.com>
2024-11-19 17:57:10 +0800

    feat:侧边功能栏、搜索框

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,51 +3,79 @@
 import Image from "next/image";
 import React from "react";
 
+import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
-import { Tag } from "@/components/tag";
+import Tag from "@/components/tag";
 export default function ListPage() {
+  const placeholders = [
+    "What's the first rule of Fight Club?",
+    "Who is Tyler Durden?",
+    "Where is Andrew Laeddis Hiding?",
+    "Write a Javascript method to reverse a string",
+    "How to assemble your own PC?",
+  ];
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
-    <div className="mb-10 flex min-h-[calc(100vh-170px)] w-full flex-wrap justify-center gap-10 last:ml-auto">
-      <Tag />
-      {[1, 2, 3, 4].map((item) => {
-        return (
-          <CardContainer key={item} className="inter-var">
-            <CardBody className="group/card relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[20rem]">
-              <CardItem
-                className="text-xl font-bold text-neutral-600 dark:text-white"
-                translateZ="50"
-              >
-                Make things float in air
-              </CardItem>
-              <CardItem
-                as="p"
-                className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"
-                translateZ="60"
-              >
-                Hover over this card to unleash the power of CSS perspective
-              </CardItem>
-              <CardItem className="mt-4 w-full" translateZ="100">
-                <Image
-                  alt="thumbnail"
-                  className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
-                  height="500"
-                  src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  width="1000"
-                />
-              </CardItem>
-              <div className="mt-5 flex items-center justify-end">
+    <div className="relative mb-10 min-h-[calc(100vh-170px)] w-full">
+      <div className="flex w-full pt-5">
+        <PlaceholdersAndVanishInput
+          placeholders={placeholders}
+          onChange={handleChange}
+          onSubmit={onSubmit}
+        />
+      </div>
+
+      <div className="fixed bottom-10 right-10">
+        <Tag />
+      </div>
+      <div className="flex flex-wrap gap-5">
+        {[1, 2, 3, 4].map((item) => {
+          return (
+            <CardContainer key={item} className="inter-var">
+              <CardBody className="group/card relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[20rem]">
                 <CardItem
-                  as="button"
-                  className="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"
-                  translateZ={20}
+                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                  translateZ="50"
                 >
-                  Try now →
+                  Make things float in air
                 </CardItem>
-              </div>
-            </CardBody>
-          </CardContainer>
-        );
-      })}
+                <CardItem
+                  as="p"
+                  className="mt-2 max-w-sm text-sm text-neutral-500 dark:text-neutral-300"
+                  translateZ="60"
+                >
+                  Hover over this card to unleash the power of CSS perspective
+                </CardItem>
+                <CardItem className="mt-4 w-full" translateZ="100">
+                  <Image
+                    alt="thumbnail"
+                    className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
+                    height="500"
+                    src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    width="1000"
+                  />
+                </CardItem>
+                <div className="mt-5 flex items-center justify-end">
+                  <CardItem
+                    as="button"
+                    className="rounded-xl bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"
+                    translateZ={20}
+                  >
+                    Try now →
+                  </CardItem>
+                </div>
+              </CardBody>
+            </CardContainer>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- components/tag.tsx
+++ components/tag.tsx
@@ -1,76 +1,63 @@
-import React from "react";
+"use client";
+import { motion } from "framer-motion";
 import {
-  IconBrandGithub,
-  IconBrandX,
-  IconExchange,
-  IconHome,
-  IconNewSection,
-  IconTerminal2,
-} from "@tabler/icons-react";
-
-import { FloatingDock } from "@/components/ui/floating-dock";
-
-export function Tag() {
-  const links = [
-    {
-      title: "Home",
-      icon: (
-        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-
-    {
-      title: "Products",
-      icon: (
-        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "Components",
-      icon: (
-        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "Aceternity UI",
-      icon: (
-        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "Changelog",
-      icon: (
-        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-
-    {
-      title: "Twitter",
-      icon: (
-        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
-    {
-      title: "GitHub",
-      icon: (
-        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-      ),
-      href: "#",
-    },
+  AiFillStar,
+  AiFillTags,
+  AiFillPlusCircle,
+  AiFillHeart,
+  AiFillUpCircle,
+  AiFillAliwangwang,
+} from "react-icons/ai";
+export default function Tag({ className }: { className?: string }) {
+  const icons = [
+    { component: AiFillPlusCircle, id: "add" },
+    { component: AiFillStar, id: "star" },
+    { component: AiFillTags, id: "tags" },
+    { component: AiFillHeart, id: "heart" },
+    { component: AiFillAliwangwang, id: "rabot" },
+    { component: AiFillUpCircle, id: "up" },
   ];
 
   return (
-    <div className="flex h-auto w-full flex-col items-center justify-center">
-      <FloatingDock
-        items={links}
-        mobileClassName="translate-y-20" // only for demo, remove for production
-      />
+    <div className={`flex h-screen items-center justify-center ${className}`}>
+      <div className="flex h-[280px] w-10 flex-col items-center justify-center gap-5 rounded-sm bg-gray-50 dark:bg-neutral-900">
+        {icons
+          .filter((item) => item.id !== "rabot") // 过滤掉 id 为 "rabot" 的图标
+          .map((item) => (
+            <motion.div
+              key={item.id}
+              className="cursor-pointer"
+              style={{ willChange: "transform" }}
+              whileHover={{
+                scale: 1.5,
+                rotate: 0,
+                transition: { duration: 0.3 },
+              }}
+            >
+              <item.component className="h-6 w-6 hover:text-primary-500" />
+            </motion.div>
+          ))}
+        {icons
+          .filter((item) => item.id === "rabot") // 过滤掉 id 为 "rabot" 的图标
+          .map((item) => (
+            <motion.div
+              key={item.id}
+              animate={{
+                y: [0, -10, 0], // 垂直方向的关键帧
+              }}
+              className="cursor-pointer"
+              style={{ willChange: "transform" }}
+              transition={{
+                duration: 1, // 每次循环的时间
+                ease: "easeInOut", // 平滑过渡
+                repeat: Infinity, // 无限循环
+                delay: 0.2, // 每个图标的动画有微小延迟
+              }}
+            >
+              <item.component className="h-6 w-6 text-primary-500" />
+            </motion.div>
+          ))}
+      </div>
     </div>
   );
 }
